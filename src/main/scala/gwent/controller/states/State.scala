a83c7cd71bff35a157454ceb1783b179bba24c18
Daniel Radrigan Barros <danielradriganb@gmail.com>
2023-06-05 17:21:41 -0400

    FEAT(State): metodos en Trait state

--- src/main/scala/gwent/controller/states/State.scala
+++ src/main/scala/gwent/controller/states/State.scala
@@ -5,4 +5,34 @@ import gwent.controller.Controller
 
 trait State {
   def setController(controller: Controller): Unit
+
+  def handle(): Unit
+  def start(): Unit
+  def roundInit(): Unit
+  def player1Turn(): Unit
+  def player2Turn(): Unit
+  def playCardP1(): Unit
+  def playCardP2(): Unit
+  def player1FinalTurn(): Unit
+  def player2FinalTurn(): Unit
+  def playCardsP1(): Unit
+  def playCardsP2(): Unit
+  def finishRound(): Unit
+  def nextRound(): Unit
+  def finishGame(): Unit
+  def restartGame(): Unit
+  def isStart(): Boolean
+  def isRoundInit(): Boolean
+  def isPlayer1Turn(): Boolean
+  def isPlayer2Turn(): Boolean
+  def isPlayCardP1(): Boolean
+  def isPlayCardP2(): Boolean
+  def isPlayer1FinalTurn(): Boolean
+  def isPlayer2FinalTurn(): Boolean
+  def isPlayCardsP1(): Boolean
+  def isPlayCardsP2(): Boolean
+  def isFinishRound(): Boolean
+  def isNextRound(): Boolean
+  def isFinishGame(): Boolean
+  def isRestartGame(): Boolean
 }
