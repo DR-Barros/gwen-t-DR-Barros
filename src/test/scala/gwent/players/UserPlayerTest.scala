23a9afd8545ffab7ea24894a4a10307e73a7e0be
Daniel Radrigan Barros <danielradriganb@gmail.com>
2023-06-23 16:44:21 -0400

    REFACTOR(playerTest): se cambia el player test

--- src/test/scala/gwent/players/UserPlayerTest.scala
+++ src/test/scala/gwent/players/UserPlayerTest.scala
@@ -1,17 +1,11 @@
 package cl.uchile.dcc
-package Players
-package Cards
+package gwent.players
 
 import gwent.cards.{Card, CorpCard, Deck, DistanceCard, SiegeCard}
-import gwent.players.UserPlayer
+import gwent.cards.effects.NullEffect
+import gwent.exception.HandDontHaveCard
 
-import cl.uchile.dcc.gwent.cards.effects.NullEffect
-import cl.uchile.dcc.gwent.exception.HandDontHaveCard
-import munit.FunSuite
-
-import scala.collection.mutable.ListBuffer
-
-class PlayerTest extends munit.FunSuite {
+class UserPlayerTest extends munit.FunSuite {
   var player1: UserPlayer = _
   var player2: UserPlayer = _
 
@@ -31,18 +25,19 @@ class PlayerTest extends munit.FunSuite {
       new DistanceCard("Din Djarin", 13, new NullEffect), new CorpCard("General Grievous", 14, new NullEffect), new DistanceCard("Leia Organa", 16, new NullEffect)
     )))
   }
-  test("Un jugador debe tener un nombre"){
+
+  test("Un jugador debe tener un nombre") {
     assertEquals(player1.getName(), "P1")
     assertEquals(player2.getName(), "P2")
   }
-  test("Un jugador debe ser de clase UserPlayer"){
+  test("Un jugador debe ser de clase UserPlayer") {
     assert(!player1.equals(new CorpCard("P1", 1, new NullEffect)))
   }
-  test("Un jugador se identifica por su nombre y zona"){
+  test("Un jugador se identifica por su nombre y zona") {
     assert(!player1.equals(player2))
     assert(player1.equals(new UserPlayer("P1", new Deck(Array()))))
   }
-  test("manejo de gemas de un jugador"){
+  test("manejo de gemas de un jugador") {
     assertEquals(player1.getGems(), 2)
     player1.loseGems()
     assertEquals(player1.getGems(), 1)
@@ -50,10 +45,10 @@ class PlayerTest extends munit.FunSuite {
     player1.loseGems()
     assertEquals(player1.getGems(), 0)
   }
-  test("Un jugador comienza con 0 cartas en su mano"){
+  test("Un jugador comienza con 0 cartas en su mano") {
     assertEquals(player1.handSize(), 0)
   }
-  test("Los jugadores pueden robar cartas del mazo y agregarla a la mano"){
+  test("Los jugadores pueden robar cartas del mazo y agregarla a la mano") {
     player1.stealCard()
     assertEquals(player1.handSize(), 1)
     player1.stealCard()
@@ -61,7 +56,7 @@ class PlayerTest extends munit.FunSuite {
     player1.stealCard()
     assertEquals(player1.handSize(), 4)
   }
-  test("El jugador puede jugar cartas y estas se eliminan de la mano"){
+  test("El jugador puede jugar cartas y estas se eliminan de la mano") {
     assertEquals(player2.handSize(), 0)
     player2.stealCard()
     player2.stealCard()
@@ -73,8 +68,10 @@ class PlayerTest extends munit.FunSuite {
     assert(card.isInstanceOf[Card])
     assertEquals(player2.handSize(), 0)
   }
-  test("Si un jugador intenta jugar una carta sin tener cartas en la mano se lanza la excepcion HandDontHaveCard"){
+  test("Si un jugador intenta jugar una carta sin tener cartas en la mano se lanza la excepcion HandDontHaveCard") {
     assertEquals(player2.handSize(), 0)
-    intercept[HandDontHaveCard]{player2.playCard(0)}
+    intercept[HandDontHaveCard] {
+      player2.playCard(0)
+    }
   }
 }
