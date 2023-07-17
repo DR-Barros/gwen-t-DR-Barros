533e60b46cf2ff93379b1b37c99546676c89ffa7
Daniel Radrigan Barros <danielradriganb@gmail.com>
2023-07-16 20:09:25 -0400

    TEST(P2playcard): se implementan los test para los estados en que P2 juega una carta)

--- src/test/scala/gwent/contoller/states/PlayCardsP2StateTest.scala
+++ src/test/scala/gwent/contoller/states/PlayCardsP2StateTest.scala
@@ -4,8 +4,8 @@ package gwent.contoller.states
 import gwent.controller.GameController
 
 import cl.uchile.dcc.gwent.board.Board
-import cl.uchile.dcc.gwent.cards.effects.NullEffect
-import cl.uchile.dcc.gwent.cards.{Card, CorpCard}
+import cl.uchile.dcc.gwent.cards.effects.{ClearEffect, NullEffect}
+import cl.uchile.dcc.gwent.cards.{Card, CorpCard, WeatherCard}
 import cl.uchile.dcc.gwent.cards.structures.Deck
 import cl.uchile.dcc.gwent.players.UserPlayer
 
@@ -26,11 +26,39 @@ class PlayCardsP2StateTest extends munit.FunSuite {
     c.handleState()
     assertEquals(c.board.get.getSec2C()(0), corp)
   }
-  test("Si el player 2 tiene mas fuerza juega una carta de unidad si tiene") {
+  test("Si el player 2 tiene mas fuerza peron no tiene una carta de unidad pasa el turo") {
     c.p2 = Some(new UserPlayer("P2", new Deck(new Array[Card](0))))
     c.board = Some(new Board)
     c.roundInit()
     c.player1Turn()
+    c.playCardsP2()
+    c.handleState()
+    assert(c.isFinishRound())
+  }
+  test("Si el player 2 tiene menos fuerza juega una carta de clima si tiene") {
+    val clima = new WeatherCard("clima",  new ClearEffect)
+    val corp = new CorpCard("corp", 9, new NullEffect)
+    c.p1 = Some(new UserPlayer("P2", new Deck(Array[Card](corp))))
+    c.p2 = Some(new UserPlayer("P2", new Deck(Array[Card](clima))))
+    c.p1.get.stealCard()
+    c.p2.get.stealCard()
+    c.board = Some(new Board)
+    c.roundInit()
+    c.player1Turn()
+    c.board.get.playCardSec1(c.p1.get, 1)
+    c.playCardsP2()
+    c.handleState()
+    assertEquals(c.board.get.getSecW()(0), clima)
+  }
+  test("Si el player 2 tiene mas fuerza peron no tiene una carta de unidad pasa el turo") {
+    val corp = new CorpCard("corp", 9, new NullEffect)
+    c.p2 = Some(new UserPlayer("P2", new Deck(new Array[Card](0))))
+    c.p1 = Some(new UserPlayer("P2", new Deck(Array[Card](corp))))
+    c.p1.get.stealCard()
+    c.board = Some(new Board)
+    c.roundInit()
+    c.player1Turn()
+    c.board.get.playCardSec1(c.p1.get, 1)
     c.playCardsP2()
     c.handleState()
     assert(c.isFinishRound())
